ccde4382a1b269e9bcb80bdc54b8c89ac90b4b00
Sukanyareddy-vydyula <sukanyareddy089@gmail.com>
2025-06-30 12:51:43 +0530

    Python programs

---
+++ Ifex.py
@@ -0,0 +1,44 @@
+
+'''
+A = int(input("A : "))
+B = int(input("B : "))
+if(A > B) :
+    print("A is Greater")
+else :
+        print("B is Greater")
+'''
+#nested if
+'''
+Marks  = int(input("Enter the marks : "))
+if(Marks >= 90):
+    print("Grade A ")
+    if(Marks < 75) :
+       print("Grade B ")
+       if(Marks 55):
+           print("Grade c ")
+       else:
+           print("Grade  d ")
+    else :
+        print("grade e")
+else:
+    print("Failed")
+'''
+#For loop
+'''
+data = [1,2,3,4,5,6,7,8,9]
+for i in data :
+    print("Data value is : ",i)
+else :
+    print("Data lsit value is completed")
+'''
+# write a python program to print the numbers 10 to 1 using while loop
+'''
+i = 10;
+while( i > 0 ):
+    print(i)
+    i = i - 1
+'''
+#functions
+def Add(a, b):
+    return (a + b)
+print(Add(20, 52))
